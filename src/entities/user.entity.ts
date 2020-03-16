b96fb5923b5793476af4b8338e14a00561447644
DarKPhuRioN <jualvalitube@gmail.com>
2020-03-15 23:52:36 -0500

    Update entities

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,17 +1,33 @@
 import { Entity, Column, ObjectIdColumn, ObjectID } from 'typeorm';
 
 import { People } from './people.entity';
+import { State } from './enums/state.enum';
 
 @Entity('User')
 export class User {
   @ObjectIdColumn()
   _id: ObjectID;
 
-  @Column({ type: 'string' })
+  @Column({
+    nullable: false,
+    type: 'string',
+  })
   username: string;
 
-  @Column()
+  @Column({
+    nullable: false,
+    type: 'string',
+  })
   password: string;
+
+  @Column({
+    nullable: false,
+    type: 'string',
+  })
+  email: string;
+
+  @Column({ nullable: false })
+  state: State;
 
   @Column(type => People)
   people: People[];
