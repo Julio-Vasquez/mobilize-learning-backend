f6a59a4928124c236baf3acba99d8f6281fa5927
Julio-Vasquez <jualvalitube@gmail.com>
2020-07-20 23:12:13 -0500

    fix(certificate): response

--- src/modules/certificate/certificate.service.ts
+++ src/modules/certificate/certificate.service.ts
@@ -56,7 +56,7 @@ export class CertificateService {
     //si existe el registro
     const { _id, generate, progress, urlCertificate, urlPreview } = certificate;
 
-    if (generate) return { certificate: urlCertificate, preview: urlPreview, generate: generate }; //ya ha sido generado, solo se retorna la url
+    if (generate) return { certificate: urlCertificate, preview: urlPreview, generate: false }; //ya ha sido generado, solo se retorna la url
     else if (progress === 100) {  //ya completo el contenido, pero nunca genero el certificado
       //generaremos el certificado chekeando que ya tenga el 100% del contenido terminado
       const html = Template(user[0].name, user[0].lastName, user[0].identification); //obtengo la plantilla html para crear el pdf
@@ -81,11 +81,11 @@ export class CertificateService {
       //le doy un time al metodo anterior por que es lento!
       setTimeout(() => {
         convert(config)
-      }, 3000);
+      }, 4000);
       //actualizo las urls
       await this.certificateModel.updateOne({ _id: _id }, { urlCertificate: fullPathPdf, generate: true, urlPreview: fullPathPng });
       //retorno las url
-      return { certificate: fullPathPdf, preview: fullPathPng, generate: false };
+      return { certificate: fullPathPdf, preview: fullPathPng, generate: true };
     } else return { error: 'PROGRESS_NOT_FULL', detail: 'No ha completado el 100% de los cursos' };//no ha completado el contenidoido generado y solicita el certificado
 
   }
